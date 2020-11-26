8954fbc4265855a5d75b620ec6774185f44f00a2
chelskeeley <chelsea.keeley@gmail.com>
2020-11-26 16:13:08 -0500

    Add createDataContext and AuthProvider

---
+++ src/context/AuthContext.js
@@ -0,0 +1,18 @@
+import createDataContext from "./createDataContext";
+
+const initialState = {
+  isSignedIn: false
+};
+
+const authReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export const { Provider, Context } = createDataContext(
+  authReducer,
+  {},
+  initialState
+);
