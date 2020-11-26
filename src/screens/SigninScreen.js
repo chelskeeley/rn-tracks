b16b330ba6d5b9be76440a33fbfbd0f59f4c4d7c
chelskeeley <chelsea.keeley@gmail.com>
2020-11-26 12:26:52 -0500

    Add basic navigation

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Button } from "react-native";
 
-const SigninScreen = () => {
-  return <Text style={{ fontSize: 48 }}>SigninScreen</Text>;
+const SigninScreen = ({ navigation }) => {
+  return (
+    <>
+      <Text style={{ fontSize: 48 }}>SigninScreen</Text>
+      <Button
+        title="Go to sign up"
+        onPress={() => navigation.navigate("Signup")}
+      />
+    </>
+  );
 };
 
 const styles = StyleSheet.create({});
