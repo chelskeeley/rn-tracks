da63421a9d976bb3e57dcfcae7301b8374abebb1
chelskeeley <chelsea.keeley@gmail.com>
2020-11-27 14:48:31 -0500

    Set up Map with dummy data coords

---
+++ src/components/TrackForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const TrackForm = () => {
+  return <Text>Track form</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default TrackForm;
