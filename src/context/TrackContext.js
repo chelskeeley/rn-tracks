edb473a46835365faca7be1c9676fe6e64c16abe
chelskeeley <chelsea.keeley@gmail.com>
2020-12-08 15:27:45 -0500

    Show list of tracks on TrackListScreen

--- src/context/TrackContext.js
+++ src/context/TrackContext.js
@@ -5,13 +5,18 @@ const INITIAL_STATE = [];
 
 const trackReducer = (state, action) => {
   switch (action.type) {
+    case "fetch_tracks":
+      return action.payload;
     default:
       return state;
   }
 };
 
 const actions = {
-  fetchTracks: (dispatch) => () => {},
+  fetchTracks: (dispatch) => async () => {
+    const response = await trackerApi.get("/tracks");
+    dispatch({ type: "fetch_tracks", payload: response.data });
+  },
   createTrack: (dispatch) => async (name, locations) => {
     await trackerApi.post("/tracks", { name, locations });
   }
